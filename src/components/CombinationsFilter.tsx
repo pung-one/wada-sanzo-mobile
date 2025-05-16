384101aecbaff7b3935eb25d95f659d8cd04440a
pung-one <p-ungerer@systemli.org>
2025-05-16 22:38:44 +0200

    add filter for combinations

---
+++ src/components/CombinationsFilter.tsx
@@ -0,0 +1,113 @@
+import { Pressable, StyleSheet, View } from "react-native";
+
+type Props = {
+  filter: 1 | 2 | 3 | 4;
+  setFilter: React.Dispatch<React.SetStateAction<1 | 2 | 3 | 4>>;
+};
+
+export function CombinationsFilter({ filter, setFilter }: Props) {
+  return (
+    <View style={styles.root}>
+      <Pressable
+        onPress={() => setFilter((prev) => (prev === 2 ? 1 : 2))}
+        style={[
+          styles.filterButton,
+          { backgroundColor: filter === 2 ? "white" : "black" },
+        ]}
+      >
+        <View
+          style={[
+            styles.divider,
+            { backgroundColor: filter === 2 ? "black" : "white" },
+          ]}
+        />
+        <View
+          style={[
+            styles.divider,
+            { backgroundColor: filter === 2 ? "black" : "white" },
+          ]}
+        />
+      </Pressable>
+
+      <Pressable
+        onPress={() => setFilter((prev) => (prev === 3 ? 1 : 3))}
+        style={[
+          styles.filterButton,
+          { backgroundColor: filter === 3 ? "white" : "black" },
+        ]}
+      >
+        <View
+          style={[
+            styles.divider,
+            { backgroundColor: filter === 3 ? "black" : "white" },
+          ]}
+        />
+        <View
+          style={[
+            styles.divider,
+            { backgroundColor: filter === 3 ? "black" : "white" },
+          ]}
+        />
+        <View
+          style={[
+            styles.divider,
+            { backgroundColor: filter === 3 ? "black" : "white" },
+          ]}
+        />
+      </Pressable>
+
+      <Pressable
+        onPress={() => setFilter((prev) => (prev === 4 ? 1 : 4))}
+        style={[
+          styles.filterButton,
+          { backgroundColor: filter === 4 ? "white" : "black" },
+        ]}
+      >
+        <View
+          style={[
+            styles.divider,
+            { backgroundColor: filter === 4 ? "black" : "white" },
+          ]}
+        />
+        <View
+          style={[
+            styles.divider,
+            { backgroundColor: filter === 4 ? "black" : "white" },
+          ]}
+        />
+        <View
+          style={[
+            styles.divider,
+            { backgroundColor: filter === 4 ? "black" : "white" },
+          ]}
+        />
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  root: {
+    position: "static",
+    flexDirection: "row",
+    paddingTop: 10,
+    paddingHorizontal: 10,
+    paddingBottom: 10,
+    gap: 10,
+  },
+  filterButton: {
+    flex: 1,
+    flexDirection: "row",
+    height: 40,
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 8,
+    overflow: "hidden",
+    gap: 1,
+  },
+  divider: {
+    flex: 1,
+    backgroundColor: "white",
+    maxWidth: "50%",
+  },
+});
--- src/screens/combinations/CombinationsScreen.tsx
+++ src/screens/combinations/CombinationsScreen.tsx
@@ -1,25 +1,51 @@
-import { FlatList, Pressable } from "react-native";
+import { FlatList, Pressable, StyleSheet, View } from "react-native";
 import { colorsWithSlug } from "../../assets/colors";
 import { createCombinationArray } from "../../utils/helper";
 import { Combination } from "../../components/Combination";
 import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
+import { CombinationsFilter } from "../../components/CombinationsFilter";
 
-const combinationArray = createCombinationArray(colorsWithSlug);
+const combinationArrayAll = createCombinationArray(colorsWithSlug);
+
+const combinationsMap = {
+  1: combinationArrayAll,
+  2: combinationArrayAll.filter(
+    (combination) => combination.combination.length === 2
+  ),
+  3: combinationArrayAll.filter(
+    (combination) => combination.combination.length === 3
+  ),
+  4: combinationArrayAll.filter(
+    (combination) => combination.combination.length === 4
+  ),
+};
 
 export function CombinationsScreen() {
+  const [filter, setFilter] = useState<1 | 2 | 3 | 4>(1);
   const { navigate } = useNavigation();
 
   return (
-    <FlatList
-      data={combinationArray}
-      keyExtractor={(c) => c.id.toString()}
-      renderItem={({ item, index }) => (
-        <Pressable
-          onPress={() => navigate("CombinationDetail", { index: index })}
-        >
-          <Combination combinationData={item} />
-        </Pressable>
-      )}
-    />
+    <View style={styles.root}>
+      <CombinationsFilter filter={filter} setFilter={setFilter} />
+
+      <FlatList
+        data={combinationsMap[filter]}
+        keyExtractor={(c) => c.id.toString()}
+        renderItem={({ item, index }) => (
+          <Pressable
+            onPress={() => navigate("CombinationDetail", { index: index })}
+          >
+            <Combination combinationData={item} />
+          </Pressable>
+        )}
+      />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  root: {
+    backgroundColor: "white",
+  },
+});
